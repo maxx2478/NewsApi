1607960b2797184921c9d3e0cc509b77d8bd9bdf
maxx2478 <64951609+maxx2478@users.noreply.github.com>
2021-06-05 18:10:54 +0530

    minor changes.

--- app/src/main/java/com/exotic/kotlinassignment/utils/KeyConstants.kt
+++ app/src/main/java/com/exotic/kotlinassignment/utils/KeyConstants.kt
@@ -2,7 +2,7 @@ package com.exotic.kotlinassignment.utils
 
 object KeyConstants  {
 
-    const val MY_KEY = "842e3d449b544b3d98b1de378598e62d"
+    const val MY_KEY = "Your_Api_Key_Here"
     //Haven't added the api key to avoid misuse on git community as this is a public repository.
 
 }
